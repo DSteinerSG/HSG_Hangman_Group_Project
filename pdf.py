26a8249b2d330e962870a7240c46f4dca1615536
lugmr <82474382+lugmr@users.noreply.github.com>
2023-05-13 23:16:57 +0200

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,11 +1,16 @@
+#import the necessary extensions
+#hier habe ich noch Probleme, das scheint auf meinem Mac nicht so zu klappen wie im Beispiel
 import jinja2
 import pdfkit
 from datetime import datetime
 
+#hier können wir die Variablen einfach an die sonst genutzten anpassen
 my_name = "Max Mustermann"
 number_of_tries = "5"
+#save date as a variable
 today_date = datetime.today().strftime("%d %b, %Y")
 
+#pass context into the code which is generating the pdf
 context = {'my_name': my_name, 'number_of_tries': number_of_tries,'today_date': today_date}
 
 template_loader = jinja2.FileSystemLoader('./')
