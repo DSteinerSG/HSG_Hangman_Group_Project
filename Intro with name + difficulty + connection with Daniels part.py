232c50a1f03638aa3125dadf8434a9dfb1a78ac9
TheRealAnonymous1 <132982728+TheRealAnonymous1@users.noreply.github.com>
2023-05-15 13:37:52 +0200

    Update Intro with name + difficulty + connection with Daniels part.py

--- Intro with name + difficulty + connection with Daniels part.py
+++ Intro with name + difficulty + connection with Daniels part.py
@@ -9,89 +9,98 @@ print('Hello' + ' ' + name + ',' + ' ' + "let's play Hangman!")
 with open('999 common english words.txt', 'r') as file:
     words = file.read().splitlines()
 
-
-# Ask the player to choose the level of difficulty (easy, medium or difficult)
+# Start a loop for the game
 while True:
-    difficulty = input('Please choose the level of difficulty: Easy (E), Medium (M) or Difficult (D) ')
     
-    if difficulty.upper() in ('E', 'M', 'D'):
-        break
+    
+    while True:
+        # Ask the player to choose the level of difficulty (easy, medium or difficult)
+        difficulty = input('Please choose the level of difficulty: Easy (E), Medium (M) or Difficult (D) ')
+    
+        # If the input is correct continue with the game
+        if difficulty.upper() in ('E', 'M', 'D'):
+            break
+        
+        # If the input is wrong, ask the player to enter a valid level of difficulty
+        else:
+            print('Wrong input. Please enter a valid level of difficulty.')
+
+    # Using conditionals, choose a random word from the corresponding list of words
+    # Depending on the difficulty level the player has a certain number of failed attempts at his/her disposal
+    # Anzahl von Fehlversuchen je Schwierigkeitsgrad noch festzulegen
+    if difficulty.upper() == 'E':
+        word = random.choice(words)
+        max_number_of_misses = 8
+
+    elif difficulty.upper() == 'M':
+        word = random.choice(words)
+        max_number_of_misses = 6
+
+    elif difficulty.upper() == 'D':
+        word = random.choice(words)
+        max_number_of_misses = 4
+
+   
+    # visible word:
+    visible = ["_" for n in range(len(word))]
+
+    # Turn-timer (difficulty-dependent):
+
+
+    # win condition:
+    won = 0
+
+    # count wrong_guesses:
+    wrong_guesses = []
+
+    # letter-list:
+    letters_possible = list("abcdefghijklmnopqrstuvwxyz")
+    letters = list("abcdefghijklmnopqrstuvwxyz")
+
+    while len(wrong_guesses) < max_number_of_misses:
+
+        print("\n\n\n")
+        print(*visible)
+        print(f"\n\nWrong guesses: {*wrong_guesses,}\n\n")
+
+        if "_" not in visible:
+            won = 1
+            break
+
+        guess = input("Please enter a letter to try: ").lower()
+
+        if guess not in letters_possible:
+            print("Please enter a valid letter.")
+
+        elif guess not in letters:
+            print("You have already guessed this letter.")
+
+        else:
+            letters.remove(guess)
+
+            for char in range(len(word)):
+
+                if guess == word[char]:
+                    visible[char] = guess
+
+            if guess not in word:
+                wrong_guesses.append(guess)
+
+
+    if won == 1:
+        print("You won the game!")
+
     else:
-        print('Wrong input. Please enter a valid level of difficulty.')
+        print("You lost the game.")
 
-# Using conditionals, choose a random word from the corresponding list of words
-# Anzahl von Fehlversuchen je Schwierigkeitsgrad noch festzulegen
-if difficulty.upper() == 'E':
-    word = random.choice(words) 
-    max_number_of_misses = 8
-
-elif difficulty.upper() == 'M':
-    word = random.choice(words)
-    max_number_of_misses = 6
-
-elif difficulty.upper() == 'D':
-    word = random.choice(words)
-    max_number_of_misses = 4
-
-else:
-    print('Error - Wrong input')
-
-
-
-# visible word:
-visible = ["_" for n in range(len(word))]
-
-# Turn-timer (difficulty-dependent):
-
-
-# win condition:
-won = 0
-
-# count wrong_guesses:
-wrong_guesses = []
-
-# letter-list:
-letters_possible = list("abcdefghijklmnopqrstuvwxyz")
-letters = list("abcdefghijklmnopqrstuvwxyz")
-
-while len(wrong_guesses) < max_number_of_misses:
-
-    print("\n\n\n")
-    print(*visible)
-    print(f"\n\nWrong guesses: {*wrong_guesses,}\n\n")
-
-    if "_" not in visible:
-        won = 1
-        break
-
-    guess = input("Please enter a letter to try: ").lower()
-
-    if guess not in letters_possible:
-        print("Please enter a valid letter.")
-
-    elif guess not in letters:
-        print("You have already guessed this letter.")
-
-    else:
-        letters.remove(guess)
-
-        for char in range(len(word)):
-
-            if guess == word[char]:
-                visible[char] = guess
-
-        if guess not in word:
-            wrong_guesses.append(guess)
-
-
-if won == 1:
-    print("You won the game!")
+    # Print the correct word
     print("The correct word was:", word)
 
-else:
-    print("You lost the game.")
-    print("The correct word was:", word)
+    # Ask the player whether he/she wants to play again.
+    # If YES, start the game where the player can choose the level of difficulty
+    # If NO, end the game and thank the player for playing Hangman
+    play_again = input('Do you want to play again? Input YES or NO: ').upper()
+    if play_again != 'YES':
+        break
 
-
-# prevents console closing:
-con = input("Thank you for playing Hangman!")
+print("Thank you for playing Hangman!")
